af2eea591ee4be093f27e8d021cb3de686b6bdea
Fetra01 <fetra.andria@outlook.fr>
2021-04-19 15:27:49 +0200

    ex

---
+++ src/features/message/messageList.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchAsync, selectMessage } from './messageSlice';
+
+export function MessageList() {
+    const messages = useSelector(selectMessage);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchAsync());
+    }, [dispatch]);
+
+    return(
+        <div>
+            {messages.map((message) => (
+                <span>{message.message}</span>
+            ))}
+        </div>
+    );
+}
